c1aa1b0ed7f1d774ed36e57ea771a02b7bf966b9
afmoyar <afmoyar@unal.edu.co>
2020-04-26 23:13:36 -0500

    first alogrithm appears to be working

--- syntactic.py
+++ syntactic.py
@@ -18,23 +18,32 @@ def is_terminal(symbol):
 #alpha is a list containing a list of terminasl or non terminal, something like a1a2a3.....an is [a1,a2,...,an]
 #where ai could be terminasl or non terminal
 def get_first(alpha,first):
+    print("alpha", alpha)
+    print("first", first)
     if(alpha==[epsilon]):
+        print("alpha is [epsilon]")
         first.add(epsilon)
         return first
     else: # string of type a1a2a3.....an, n>=1
+        print("string of type a1a2a3.....an, n>=1")
         a_1 =alpha[0]
         if(is_terminal(a_1)): # string of type a1 where a1 is terminal
+            print("string of type a1 where a1 is terminal")
             first.add(a_1)
             return first
         elif (len(alpha)==1): # string of type a1 where a1 is nonterminal
-            for rule in grammar[alpha]:
+            print("string of type a1 where a1 is nonterminal")
+            for rule in grammar[a_1]:
                 get_first(rule,first)
+            return first
         else: #string of type a1a2a3.....an, n>1 where a1 is nonterminal
+            print("string of type a1a2a3.....an, n>1 where a1 is nonterminal")
             #add first(a1) - {epislon} to first(alpha)
             first_of_a1=get_first([a_1],first)
             first = first.union(first_of_a1)
             first.discard(epsilon)
             if(epsilon in first_of_a1): #if epsilong belongs to first(a1)
+                print("if epsilong belongs to first(a1)")
                 #add first(a2a3....an)  to first(alpha)
                 first.add(get_first(alpha[1:],first))
             
@@ -56,5 +65,6 @@ grammar ={
     ]
 }
 first= set()
-first = get_first("A",first)
+first = get_first(["C"],first)
+print("----------------------------------------------------")
 print(first)
