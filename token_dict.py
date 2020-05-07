8ea90ecab12596fcb0b935879dab4016955cdd8a
meguerreroa <36995005+meguerreroa@users.noreply.github.com>
2020-05-07 12:41:17 -0500

    token_list added

---
+++ token_dict.py
@@ -0,0 +1,29 @@
+
+token_list = {
+    "tk_suma" : "+",
+    "tk_mult" : "*",
+    "tk_remainder" : "%",
+    "tk_par_izq" : "(",
+    "tk_par_der" : ")",
+    "tk_cor_izq" : "[",
+    "tk_cor_der" :  "]",
+    "tk_coma" : ",",
+    "tk_dos_puntos" : ":",
+    "tk_punto" : ".",
+    "tk_menor_igual" : "<=",
+    "tk_menor" : "<",
+    "tk_mayor_igual" : ">=",
+    "tk_mayor" : ">",
+    "tk_comparacion" : "==",
+    "tk_asig" : "=",
+    "tk_ejecuta" : "->",
+    "tk_menos" : "-",
+    "tk_distinto" :  "!=",
+    "tk_division" :  "//",
+    "tk_ident" :  "IDENT",
+	"tk_newline" : "NEWLINE",
+    "tk_dedent" : "DEDENT",
+    "tk_cadena" : "STRING",
+    "tk_entero" : "INTEGER"
+
+}
